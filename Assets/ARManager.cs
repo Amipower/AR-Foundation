5610577dcffcaaad24c1ed99aa11425a9b810dc6
Amipower <71679909+Amipower@users.noreply.github.com>
2020-11-24 12:01:52 +0800

    點擊控制

---
+++ Assets/ARManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARManager : MonoBehaviour
+{
+    [Header("點擊後要生成的物件")]
+    public GameObject obj;
+    [Header("AR 管理器")]
+    public ARRaycastManager arManager;
+    public Vector2 pointMouse;
+    public List<ARRaycastHit> hits;
+    public void Tap() 
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            pointMouse = Input.mousePosition;
+            print(pointMouse);
+        }
+    }
+    public void Update()
+    {
+        Tap();
+    }
+}
